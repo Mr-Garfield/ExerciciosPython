a47795440ae0a6351f56cf2ca648d3edf7222701
Mr. Garfield <victorhlo12@hotmail.com>
2021-06-09 14:30:16 -0300

    Falta Finalizar
    O exercício necessita de conhecimento na conversão de bases numéricas.

---
+++ ExerciciosPython/ex037.py
@@ -0,0 +1,19 @@
+'''
+Projeto: Conversão de base numérica
+'''
+val = float(input('Digite um valor que deseja converter: '))
+print('-'*20)
+print('1 - Binário')
+print('2 - Octal')
+print('3 - Hexadecimal')
+print('')
+print('Escolha uma das opções ACIMA!!!')
+print('-'*20)
+base = int(input('Escolha uma das opções para converter: '))
+
+if base == 1:
+    ...
+elif base == 2:
+    ...
+elif base == 3:
+    ...
