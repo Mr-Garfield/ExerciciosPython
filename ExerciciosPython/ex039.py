f7abaa6cb514ab631520a69ef6b378f5c72ac19b
Mr. Garfield <victorhlo12@hotmail.com>
2021-06-10 19:29:46 -0300

    Finalizado
    Projeto: Alistamento Militar

---
+++ ExerciciosPython/ex039.py
@@ -0,0 +1,12 @@
+'''
+Projeto: Alistamento Militar
+'''
+from datetime import date
+nascimento = int(input('Digite seu ano de nascimento: '))
+idade = date.today().year - nascimento
+if idade <= 17:
+    print('Você não precisa se alistar agora, ainda faltam {} anos.'.format(18 - idade))
+elif idade == 18:
+    print('Já está na idade de se alistar. AVANTE SOLDADO!!!')
+else:
+    print('CORRA PARA SE ALISTAR!!! Você já têm {} anos, deveria ter se alistado com 18.'.format(idade))
