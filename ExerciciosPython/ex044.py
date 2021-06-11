5718e7d95138c84a63bb9a9e18600bd9a9abc2e0
Mr. Garfield <victorhlo12@hotmail.com>
2021-06-10 22:34:18 -0300

    Inclusão das estruturas condicionais

--- ExerciciosPython/ex044.py
+++ ExerciciosPython/ex044.py
@@ -1,13 +1,31 @@
 '''
 Projeto: Gerenciador de pagamentos
 '''
-value = ('Qual o valor do produto: ')
-
+value = float(input('Qual o valor do produto: '))
+print('')
 print('='*70)
 print('                     MÉTODOS DE PAGAMENTOS')
 print('='*70)
-print('1 - À vista Dinheiro / Cheque: 10% DESCONTO')
-print('2 - À vista Cartão: 5% DESCONTO')
-print('3 - 2x no Cartão sem juros')
-print('4 - A partir de 3x no Cartão, acrécimo de 20% do valor do produto.')
+print('[1] - À vista Dinheiro / Cheque: 10% DESCONTO')
+print('[2] - À vista Cartão: 5% DESCONTO')
+print('[3] - 2x no Cartão sem juros')
+print('[4] - A partir de 3x no Cartão, acrécimo de 20% do valor do produto.')
 print('-'*70)
+print('ESCOLHA UM DOS MÉTODOS DE PAGAMENTO ACIMA!')
+metedo = int(input('DIGITE UM NÚMERO: '))
+print('')
+print('PROCESSANDO...')
+# Tempo
+if metedo == 1:
+    ...
+elif metedo == 2:
+    ...
+elif metedo == 3:
+    ...
+elif metedo == 4:
+    ...
+else:
+    print('Escolha uma opção válida!!')
+
+print('VOLTE SEMPRE S2!!!')
+
