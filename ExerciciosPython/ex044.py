3da4c10fb1ca552e462cd67904ffd3b392fd584b
Mr. Garfield <victorhlo12@hotmail.com>
2021-06-10 21:39:54 -0300

    Falta Finalizar

---
+++ ExerciciosPython/ex044.py
@@ -0,0 +1,13 @@
+'''
+Projeto: Gerenciador de pagamentos
+'''
+value = ('Qual o valor do produto: ')
+
+print('='*70)
+print('                     MÉTODOS DE PAGAMENTOS')
+print('='*70)
+print('1 - À vista Dinheiro / Cheque: 10% DESCONTO')
+print('2 - À vista Cartão: 5% DESCONTO')
+print('3 - 2x no Cartão sem juros')
+print('4 - A partir de 3x no Cartão, acrécimo de 20% do valor do produto.')
+print('-'*70)
