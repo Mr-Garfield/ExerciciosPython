87a45b7973b4967251378bd29782f764056ecea9
Mr. Garfield <victorhlo12@hotmail.com>
2021-06-11 23:23:48 -0300

    Falta Finalizar
    Inclusão das estruturas condicionais

---
+++ ExerciciosPython/ex045.py
@@ -0,0 +1,15 @@
+'''
+Projeto: GAME - Pedra, Papel e Tesoura.
+'''
+print('-'*15)
+print('[1] - Pedra')
+print('[2] - Papel')
+print('[3] - Tesoura')
+print('-'*15)
+opcao = int(input('QUAL SUA ESCOLHA: '))
+if opcao == 1:
+    ...
+elif opcao == 2:
+    ...
+elif opcao == 3:
+    ...
