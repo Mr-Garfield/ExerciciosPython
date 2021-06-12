64f25a41ca6bc5702486098d270611cfeb3f7e8f
Mr. Garfield <victorhlo12@hotmail.com>
2021-06-12 12:12:37 -0300

    Feito
    Projeto: Conversão de bases numéricas

--- ExerciciosPython/ex037.py
+++ ExerciciosPython/ex037.py
@@ -1,7 +1,7 @@
 '''
-Projeto: Conversão de base numérica
+Projeto: Conversão de bases numéricas
 '''
-val = float(input('Digite um valor que deseja converter: '))
+val = int(input('Digite um valor que deseja converter: '))
 print('-'*20)
 print('1 - Binário')
 print('2 - Octal')
@@ -12,8 +12,10 @@ print('-'*20)
 base = int(input('Escolha uma das opções para converter: '))
 
 if base == 1:
-    ...
+    print('{} convertido para BINÁRIO {}'.format(val, bin(val)))
 elif base == 2:
-    ...
+    print('{} convertido para OCTAL {}'.format(val, oct(val)))
 elif base == 3:
-    ...
+    print('{} convertido para HEXADECIMAL {}'.format(val, hex(val)))
+else:
+    print('Opção inválida, tente novamente.')
