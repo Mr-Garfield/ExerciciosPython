36ac301b6bfe449cc20f01e9d2c9034c62dadcb8
Mr. Garfield <victorhlo12@hotmail.com>
2021-06-13 17:36:55 -0300

    Feito
    Projeto: Soma de pares

--- ExerciciosPython/ex050.py
+++ ExerciciosPython/ex050.py
@@ -1,3 +1,9 @@
 '''
-Projeto:
+Projeto: soma de pares
 '''
+somador = 0
+for c in range(1, 7):
+    n = float(input('Digite um número: '))
+    if n % 2 == 0:
+        somador = somador + n
+print(somador)
