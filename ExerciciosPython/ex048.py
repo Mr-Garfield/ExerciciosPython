bf82733b885c42326346738368621763e0a3915f
Mr. Garfield <victorhlo12@hotmail.com>
2021-06-13 16:57:03 -0300

    Feito
    Projeto: Soma de ímpares múltiplos de 3

--- ExerciciosPython/ex048.py
+++ ExerciciosPython/ex048.py
@@ -1,3 +1,9 @@
 '''
-Projeto:
+Projeto: Soma de ímpares múltiplos de 3
 '''
+soma = 0
+for n in range(1, 501):
+    if n % 2 != 0 and n % 3 == 0:
+        soma = soma + n
+        print(n)
+print('A dos ímpares múltipos de 3 vale {}'.format(soma))
