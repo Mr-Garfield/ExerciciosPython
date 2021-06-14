283c4072ad826a2b7638301e8d2da010a3bd65c7
Mr. Garfield <victorhlo12@hotmail.com>
2021-06-14 19:02:01 -0300

    Falta Finalizar

--- ExerciciosPython/ex052.py
+++ ExerciciosPython/ex052.py
@@ -1,3 +1,17 @@
 '''
-Projeto:
+Projeto: Números primos
 '''
+n = float(input('Digite um número para saber se ele é primo ou não: '))
+primo = False
+for c in range(1, 9):
+
+    if n != c and n % c == 0:
+        #Não é primo
+        primo = False
+    else:
+        primo = True
+
+if primo == True:
+    print('{} é um número primo'.format(n))
+else:
+    print('{} NÃO é um número primo.'.format(n))
