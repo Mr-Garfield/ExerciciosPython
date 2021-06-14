48c927607f808af757de4cab62afb56556f0a608
Mr. Garfield <victorhlo12@hotmail.com>
2021-06-14 19:01:41 -0300

    Falta Finalizar

--- ExerciciosPython/ex055.py
+++ ExerciciosPython/ex055.py
@@ -1,3 +1,14 @@
 '''
-Projeto:
+Projeto: Maior e Menor Peso
 '''
+novaMassa = 0
+menorMassa =0
+maiorMassa = 0
+for p in range(1,6):
+    massa = float(input('Digite sua massa corporal: '))
+    novaMassa = massa
+    if massa > novaMassa:
+        maiorMassa = massa
+    elif massa < novaMassa:
+        menorMassa = massa
+    print('A maior massa corporal é {} e a menor {}'.format(maiorMassa, menorMassa))
