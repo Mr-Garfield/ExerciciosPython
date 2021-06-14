b4731bd0b84bc201c51feea5ef20e099153ce32c
Mr. Garfield <victorhlo12@hotmail.com>
2021-06-14 16:33:34 -0300

    Feito
    Projeto: Grupo Maioridade

--- ExerciciosPython/ex054.py
+++ ExerciciosPython/ex054.py
@@ -5,6 +5,6 @@ from datetime import date
 contador = 0
 for ano in range(1,7):
     nascimento = int(input('Digite o ano em que você nasceu: '))
-    if date.today().year - nascimento >= 18:
+    if date.today().year - nascimento >= 21:
         contador += 1
 print('{} pessoas já atingiram a maioridade!'.format(contador))
