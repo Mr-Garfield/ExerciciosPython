6a4300c5c38a5c585d16d668c1aa65369aa40004
Mr. Garfield <victorhlo12@hotmail.com>
2021-06-15 20:35:35 -0300

    Feito
    Projeto: Maior e menor peso

--- ExerciciosPython/ex052.py
+++ ExerciciosPython/ex052.py
@@ -1,17 +1,39 @@
 '''
 Projeto: Números primos
 '''
-n = float(input('Digite um número para saber se ele é primo ou não: '))
-primo = False
-for c in range(1, 9):
+'''
+boa, OK, acho que sim
+'''
+from time import sleep
 
-    if n != c and n % c == 0:
-        #Não é primo
-        primo = False
+
+def processando():
+    print('PROCESSANDO', end='') #Vou colocar o enunciado na tela
+    for time in range(3):
+        sleep(0.5)
+        print('.', end='')
+
+
+contador = 0
+
+while True:
+    n = int(input('Digite um número para saber se ele É ou NÃO primo: '))
+    if n == 0:
+        break
     else:
-        primo = True
+        for i in range(2, n):
+            if n % i == 0:
+                contador += 1
+        if contador == 0:
+            print('{} É um número primo'.format(n))
+        else:
+            print('{} Não é um número primo'.format(n))
+        print("Digite '0' para finalizar o programa")
+print()
+processando()
+print()
+sleep(2)
+print('Foi um prazer ter você conosco, continue estudando :)!')
 
-if primo == True:
-    print('{} é um número primo'.format(n))
-else:
-    print('{} NÃO é um número primo.'.format(n))
+
+
