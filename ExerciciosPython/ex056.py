28767a23857ef5a82dccba7423df2ec25508c91f
Mr. Garfield <victorhlo12@hotmail.com>
2021-06-17 13:44:48 -0300

    Falta Finalizar

--- ExerciciosPython/ex056.py
+++ ExerciciosPython/ex056.py
@@ -1,6 +1,15 @@
 '''
 Projeto: Analisador Completo
 '''
-lista = [1, 2, 8, 5]
-maximo = lista.index((max(lista)))
-print(maximo)
+for c in range(1,5):
+    print(f'{c}° Pessoa:')
+    n = input('Digite seu nome: ')
+    i = int(input('Digite sua idade: '))
+    print('-'*15)
+    print('[1] MASCULINO')
+    print('[2] FEMININO')
+    print('[3] OUTRO')
+    print('-' * 15)
+    g = int(input('Digite seu gênero: '))
+    print('')
+print()
