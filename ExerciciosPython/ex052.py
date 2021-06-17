49b9dbcd01ac126b77dc4689b98faa906e8470bc
Mr. Garfield <victorhlo12@hotmail.com>
2021-06-16 22:20:45 -0300

    Feito
    Projeto: Números Primos

--- ExerciciosPython/ex052.py
+++ ExerciciosPython/ex052.py
@@ -1,14 +1,12 @@
 '''
 Projeto: Números primos
 '''
-'''
-boa, OK, acho que sim
-'''
+
 from time import sleep
 
 
 def processando():
-    print('PROCESSANDO', end='') #Vou colocar o enunciado na tela
+    print('PROCESSANDO', end='')
     for time in range(3):
         sleep(0.5)
         print('.', end='')
@@ -24,7 +22,7 @@ while True:
         for i in range(2, n):
             if n % i == 0:
                 contador += 1
-        if contador == 0:
+        if contador <= 2:
             print('{} É um número primo'.format(n))
         else:
             print('{} Não é um número primo'.format(n))
