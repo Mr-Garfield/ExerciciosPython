40bd39ddd269c7171f33ea7c908b3471f86ad06f
Mr. Garfield <victorhlo12@hotmail.com>
2021-06-16 22:58:22 -0300

    Feito
    Projeto: Maior e Menor Peso

--- ExerciciosPython/ex055.py
+++ ExerciciosPython/ex055.py
@@ -6,6 +6,6 @@ massa_lista = []
 for p in range(1,6):
     massa = float(input('Digite seu peso: '))
     massa_lista.append(massa)
-    
+
 print('O menor peso digitado foi {:.2f}kg'.format(min(massa_lista)))
 print('E o maior ditado foi {:.2f}kg'.format(max(massa_lista)))
