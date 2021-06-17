09b3cd6240ba84bacbe226e6b1423495e929bdaf
Mr. Garfield <victorhlo12@hotmail.com>
2021-06-16 22:24:31 -0300

    Feito
    Projeto: Verificador de Palíndromo

--- ExerciciosPython/ex053.py
+++ ExerciciosPython/ex053.py
@@ -1,3 +1,9 @@
 '''
-Projeto:
+Projeto: Verificador de Palíndromo
 '''
+p = input('Digite uma frase para verificar se é um palíndromo: ').replace(' ', '')
+
+if p == p[::-1]:#Lê de traz para frente
+    print('É um palíndromo!')
+else:
+    print('Não é um palíndromo!')
