446843625bb3118b38efba8efaca646524f2e575
Mr. Garfield <victorhlo12@hotmail.com>
2021-06-19 18:10:39 -0300

    Feito
    Projeto: Progressão Aritmética

--- ExerciciosPython/ex051.py
+++ ExerciciosPython/ex051.py
@@ -8,4 +8,5 @@ print('')
 print('Os 10 primeiros termos da P.A são: ', end='')
 for n in range(1, 11):
     an = a1 + (n - 1) * r
-    print(f'{an:.0f}; ', end='')
+    print(f'{an:.0f} ➝ ', end='')
+print('...')
