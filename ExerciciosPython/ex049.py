53bed415f7a6a8316ec3aaecbe451d1d630436a1
Mr. Garfield <victorhlo12@hotmail.com>
2021-06-19 17:58:41 -0300

    Feito
    Tabuada v2.0

--- ExerciciosPython/ex049.py
+++ ExerciciosPython/ex049.py
@@ -2,7 +2,5 @@
 Projeto: Tabuada v2.0
 '''
 n = int(input('Digite um número para ver sua tabuada: '))
-
 for t in range(1,11):
-    tabuada = n * t
-    print('{} x {} = {}'.format(n, t, tabuada))
+    print('{} x {} = {}'.format(n, t, n*t))
