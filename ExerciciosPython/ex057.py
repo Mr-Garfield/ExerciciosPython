4857b5611d20878abd5ad870307f86d037693011
Mr. Garfield <victorhlo12@hotmail.com>
2021-06-22 22:21:46 -0300

    Feito
    Projeto: Identificador de Gênero

--- ExerciciosPython/ex057.py
+++ ExerciciosPython/ex057.py
@@ -1,2 +1,7 @@
-
-s = input('Digite seu sexo: [M/F] ').lower()
+'''
+Projeto: Identificador de Gênero
+'''
+s = str(input('Digite seu sexo: [M/F] ')).strip().upper()
+while s not in 'MF':
+    s = input('Tente novamente: [M/F] ').upper()
+print('Sexo {} registrado!'.format(s))
