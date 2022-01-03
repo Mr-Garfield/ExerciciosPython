27f17ac44b10f7d4deae0791a4c5eee53d68ead5
Victor Lourenco <victorhlo12@hotmail.com>
2022-01-03 10:58:30 -0300

    somar-numeros-digitados

---
+++ ExerciciosPython/ex003.py
@@ -0,0 +1,8 @@
+'''
+Projeto: Somar os números digitados
+'''
+
+n1 = int(input('Digite um valor inteiro: '))
+n2 = int(input('Digite outro valor inteiro: '))
+soma = n1 + n2
+print('A soma de {} + {} = {}'.format(n1, n2, soma))
