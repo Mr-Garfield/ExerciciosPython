aabd6bc75364923dd9c45d25cdce2a62ce617fe1
Victor Lourenco <victorhlo12@hotmail.com>
2022-01-03 17:50:11 -0300

    dobro-triplo-raiz

---
+++ ExerciciosPython/ex006.py
@@ -0,0 +1,12 @@
+'''
+Projeto: Dobro, Triplo e  Raiz Quadrada
+'''
+
+n = int(input('Digite um número: '))
+dobro = n * 2
+triplo = n * 3
+raiz = n ** 0.5
+
+print('O dobro do número digitado é: {}'.format(dobro))
+print('O triplo: {}'.format(triplo))
+print('A raiz: {:.2f}'.format(raiz))
