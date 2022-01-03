7487e56e1304dddb82697f36a9b9e2f19afa5b06
Victor Lourenco <victorhlo12@hotmail.com>
2022-01-03 11:34:57 -0300

    antecessor-sucessor

---
+++ ExerciciosPython/ex005.py
@@ -0,0 +1,7 @@
+'''
+Projeto: Mostra o antecessor e sucessor de um número inteiro
+'''
+
+n = int(input('Digite um número inteiro: '))
+
+print('O seu antecessor é {} e seu sucessor {}.'.format(n-1, n+1))
