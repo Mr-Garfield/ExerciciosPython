5b0f58600edaeca4900d3114c4542fa368ebe17b
Victor Lourenco <victorhlo12@hotmail.com>
2022-01-03 19:04:19 -0300

    media-aritmetica

---
+++ ExerciciosPython/ex007.py
@@ -0,0 +1,8 @@
+'''
+Projeto: Média aritmética
+'''
+
+a1 = float(input('Primeira nota do aluno: '))
+a2 = float(input('Segunda nota do aluno: '))
+m = (a1 + a2) / 2
+print('A média entre {} e {} é igual a {:.2f}'.format(a1, a2, m))
