0fb21d2aae2765766bc65242c34c700cd9319e1b
Victor Lourenco <victorhlo12@hotmail.com>
2022-01-04 14:13:26 -0300

    parte-inteira-numero

---
+++ ExerciciosPython/ex016.py
@@ -0,0 +1,7 @@
+'''
+Projeto: Mostrar a parte inteira do número digitado
+'''
+
+numero = float(input('Digite um número: '))
+
+print('O número {} tem a parte inteira {}.'.format(numero, int(numero)))
