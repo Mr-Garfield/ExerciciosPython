93cf5b0ed935de6ec5ce67af1bb660ca04166edb
Victor Lourenco <victorhlo12@hotmail.com>
2022-01-10 19:23:05 -0300

    comparando-numeros

---
+++ ExerciciosPython/ex038.py
@@ -0,0 +1,16 @@
+'''
+Projeto: Comparando números
+'''
+
+value1 = float(input('Digite o primeiro valor: '))
+value2 = float(input('Digite o segundo valor: '))
+
+if value1 > value2:
+    print('O PRIMEIRO valor é maior!')
+elif value2 > value1:
+    print('O SEGUNDO valor é maior!')
+else:
+    print('Ambos os valores são IGUAIS!')
+
+
+
